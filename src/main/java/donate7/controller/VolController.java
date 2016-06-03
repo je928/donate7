651aa46a9016aa1073090b17964cf6b2fae1bbc7
강진우 <cuteretti@naver.com>
2016-06-03 19:27:58 +0900

    봉사 관련 !

--- src/main/java/donate7/controller/VolController.java
+++ src/main/java/donate7/controller/VolController.java
@@ -313,4 +313,13 @@ public class VolController {
 			model.addAttribute("mypgm", "../../calendar/timeList.jsp");
 			return "module/main";
 		}
+	@RequestMapping("View")
+	public String View(int pageNum, int vt_no,HttpSession session,Model model){
+		Recruit rc = vs.selectRcByVt_no(vt_no);
+		model.addAttribute("pageNum", pageNum);
+		model.addAttribute("rc", rc);
+		model.addAttribute("pgm", "../vt/vSearch/vol_tamp.jsp");
+		model.addAttribute("mypgm", "../../calendar/View.jsp");
+		return "module/main";
+	}
 	}
